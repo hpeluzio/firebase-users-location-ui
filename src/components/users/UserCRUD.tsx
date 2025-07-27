f42eba97bc0155c17d6c115db253c9ec3ac0436e
Henrique Peluzio <henriquepeluzio@Henriques-iMac.local>
2025-07-27 12:26:51 -0300

    add right-side modal drawer for user form with Headless UI

--- src/components/users/UserCRUD.tsx
+++ src/components/users/UserCRUD.tsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import type { User, CreateUserRequest, UpdateUserRequest } from '../../types/user';
 import UserList from './UserList';
 import UserForm from './UserForm';
 import { useUsers } from '../../hooks/useUsers';
+import { Dialog, Transition } from '@headlessui/react';
 
 const PAGE_SIZE_OPTIONS = [5, 10, 20];
 
@@ -114,17 +115,51 @@ const UserCRUD: React.FC = () => {
             onDelete={handleDeleteUser}
           />
         </div>
-        {isFormVisible && (
-          <div className="lg:w-96">
-            <UserForm
-              user={editingUser}
-              onSubmit={handleFormSubmit}
-              onCancel={handleFormCancel}
-              isLoading={isSaving}
-            />
-          </div>
-        )}
       </div>
+      {/* Drawer Modal for UserForm */}
+      <Transition.Root show={isFormVisible} as={Fragment}>
+        <Dialog as="div" className="relative z-50" onClose={handleFormCancel}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-30 transition-opacity" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-300"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-200"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="w-screen max-w-md">
+                    <div className="h-full flex flex-col bg-white shadow-xl">
+                      <div className="flex-1 overflow-y-auto p-6">
+                        <UserForm
+                          user={editingUser}
+                          onSubmit={handleFormSubmit}
+                          onCancel={handleFormCancel}
+                          isLoading={isSaving}
+                        />
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   );
 };
